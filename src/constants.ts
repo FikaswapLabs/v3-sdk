4ef275a7e7b0bd109683e29f414f97d920041d69
ironhark-ta <ironhawk033@gmail.com>
2023-10-31 10:37:31 +0800

    add linea mainnet

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,7 @@ export function FACTORY_ADDRESS(chainId: number): string {
     return '0xc713b22Dcce8DD9E46C3b886848d378d1d0C22A8'
   }
   else if (chainId == 59144) {
-    return '0x6C0C828200ecA0E2562D77b62d0eB5477477E22D'
+    return '0xB4bAed9529E637EB33F7433199131cD4D48D53c5'
   }
   else if (chainId == 59140) {
     return '0x6C0C828200ecA0E2562D77b62d0eB5477477E22D'
@@ -26,7 +26,7 @@ export function POOL_INIT_CODE_HASH(chainId: number): string {
     return '0x010013efa2e0a6b8508ba65ba8e424c01dd8fa715bac96ab05c5ed82b0e88754'
   }
   else if (chainId == 59144) {
-    return '0xc632952ed4748051b28453b8d940bf7a75c3f51c69ba2ac6676fdc36deeb9b56'
+    return '0x11cafa679c6c56f22f62639340e6f1e9ecf621993f96bc3740011e4bb7f6c647'
   }
   else if (chainId == 59140) {
     return '0xc632952ed4748051b28453b8d940bf7a75c3f51c69ba2ac6676fdc36deeb9b56'
